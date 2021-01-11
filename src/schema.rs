5c72da85e1e1b48d92272710bd7db874720d7a63
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-12 04:20:58 +0900

    add schema

---
+++ src/schema.rs
@@ -0,0 +1,22 @@
+table! {
+    posts (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+        user_id -> Nullable<Int4>,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        name -> Varchar,
+        image -> Nullable<Varchar>,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
